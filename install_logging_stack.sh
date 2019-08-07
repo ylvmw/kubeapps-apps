18a2f7470acb57bba5060164ad8c6ab58175f0a7
ylvmw <yngliu@vmware.com>
2019-08-07 11:25:59 +0800

    complete logging stack

--- install_logging_stack.sh
+++ install_logging_stack.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-kubectl create -f logging_pv.yaml
-helm install --name elasticsearch --namespace logging -f elasticsearch.yaml stable/elasticsearch
-#helm install --name fluentd --namespace logging stable/fluentd
+#kubectl create -f logging_pv.yaml
+helm install --name elasticsearch --namespace logging -f elasticsearch.yaml bitnami/elasticsearch
+helm install --name kibana --namespace logging -f kibana.yaml stable/kibana
+helm install --name fluent-bit --namespace logging -f fluent-bit.yaml stable/fluent-bit
+#helm install --name fluentd --namespace logging -f fluentd.yaml stable/fluentd
--- uninstall_logging_stack.sh
+++ uninstall_logging_stack.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+#helm delete --purge fluentd
+helm delete --purge fluent-bit
 helm delete --purge kibana
 helm delete --purge elasticsearch
 kubectl delete namespace logging
-kubectl delete -f logging_pv.yaml
+#kubectl delete -f logging_pv.yaml
